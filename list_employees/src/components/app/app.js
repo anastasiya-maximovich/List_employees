a90472e4fddb4f30b21f209db3aebbb8f3664c74
anastasiya025 <anastasiya025.am@gmail.com>
2021-11-15 22:22:17 +0300

    refactor: add props

--- list_employees/src/components/app/app.js
+++ list_employees/src/components/app/app.js
@@ -3,8 +3,13 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
-
 import './app.css';
+
+const data = [
+  {name: "Arizona Robbins", salary: 1200, increase: true},
+  {name:"Kally Torez", salary: 970, increase: false},
+  {name: "Darek Shepard", salary: 1100, increase: false}
+]
 
 function App() {
   return (
@@ -16,7 +21,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
